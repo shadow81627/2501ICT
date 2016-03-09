ae07f11de0e39790895d1d64e66ffe4e85cd6f09
s5014219 <s5014219@n44p234z027704.student.ad.griffith.edu.au>
2016-03-09 12:59:41 +1000

    Initial Commit

---
+++ Personal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Personal
+//
+//  Created by s5014219 on 9/03/2016.
+//  Copyright © 2016 s5014219. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
