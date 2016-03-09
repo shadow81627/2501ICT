97093a4e8bd9c75ab40714cdcbe5cc16f3edf891
s5014219 <s5014219@n44p234z027704.student.ad.griffith.edu.au>
2016-03-09 15:53:47 +1000

    added view and controllers

--- Personal/Person.swift
+++ Personal/Person.swift
@@ -7,23 +7,31 @@
 //
 
 import Foundation
-
+/**
+* THe person class
+*/
 class Person {
     var firstName: String
     var lastName: String
+    var age : Int
     var middleName: String?
-    
+    /**
+    * The initator for the Person class 
+    */
     init(firstName: String, lastName: String, age: Int, middleName: String? = nil){
         self.firstName = firstName
         self.lastName = lastName
+        self.age = age
         self.middleName = middleName
     }
-    
+    /**
+    * The to string function
+    */
     func fullName() -> String {
         if middleName != nil {
-            return firstName + middleName! + lastName
+            return firstName + middleName! + lastName + "\(age)"
         }else {
-            return firstName + lastName
+            return firstName + lastName + "\(age)"
         }
     }
 }
--- Personal/ViewController.swift
+++ Personal/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNameField: UITextField!
+    @IBOutlet weak var lastNameField: UITextField!
+    @IBOutlet weak var middleNameField: UITextField!
+    @IBOutlet weak var ageField: UITextField!
+    @IBOutlet var details: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet var firstName: UITextField!
 
+    @IBAction func fullDetail(sender: UIButton) {
+        let person = Person(firstName: firstNameField.text!,
+                            lastName: lastNameField.text!,
+                            age:  Int(ageField.text!)!,
+                            middleName: middleNameField.text)
+        details.text = person.fullName()
+    }
 }
 
