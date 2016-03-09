ee7c60d6ae43c29250f7f6b32d718b02273c68f6
s5014219 <s5014219@n44p234z027704.student.ad.griffith.edu.au>
2016-03-09 13:23:03 +1000

    Initialisation of person class

---
+++ Personal/Person.swift
@@ -0,0 +1,14 @@
+//
+//  Person.swift
+//  Personal
+//
+//  Created by s5014219 on 9/03/2016.
+//  Copyright © 2016 s5014219. All rights reserved.
+//
+
+import Foundation
+
+class Person {
+    
+    init(firstName: String, lastName: String, age: Int, middleName: String? = nil)
+}
