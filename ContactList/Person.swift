002c2d02d3bb66a538bd427e138c528d42addb8e
s5014219 <s5014219@n44p234z027709.student.ad.griffith.edu.au>
2016-04-13 12:39:24 +1000

    change comments

--- ContactList/Person.swift
+++ ContactList/Person.swift
@@ -9,15 +9,17 @@
 import Foundation
 
 
-// The person class
+// The person class used to store the detials of a person such as name and age
 class Person {
     
+    //the first name of a person
     var firstName: String
+    //the last name of a person
     var lastName: String
     
-    // The date of birth of a person is used to derive the age of a person using the current date 
+    // The date of birth of a person, can be used to derive the age of a person
     var yearOfBirth: Int?
-    // The age of a person, derived from the date of birth of a person
+    // The age of a person, derived from the date of birth of a person using the current date
     var age : Int{
         get{
             let currentDate = NSDate(),
@@ -35,7 +37,7 @@ class Person {
     // The middle name of a person
     var middleName: String?
     
-    // The initator for the Person class
+    // The initator for the Person class that uses yearOfBirth to calculate age
     init(firstName: String, lastName: String, yearOfBirth: Int? = nil, middleName: String? = nil){
         self.firstName = firstName
         self.lastName = lastName
@@ -43,13 +45,13 @@ class Person {
         self.middleName = middleName
     }
     
-    // Secondary Initaliser for the Person class
+    // Secondary Initaliser for the Person class that takes age as an imput instead of yearOfBirth
     convenience init(firstName: String, lastName: String, age: Int, middleName: String? = nil){
         self.init(firstName: firstName, lastName: lastName, yearOfBirth: age, middleName: middleName)
 
     }
     
-    // The to string function
+    // The fullName function returns the full name and age of a person
     func fullName() -> String {
         if middleName != nil {
             return firstName + middleName! + lastName + "\(age)"
