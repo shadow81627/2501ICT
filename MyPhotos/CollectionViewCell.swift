df4931b3ef06e446970a1d76f8cca14772ccde35
s5014219 <s5014219@n44p234z027701.student.ad.griffith.edu.au>
2016-04-20 13:28:55 +1000

    added collection cell

---
+++ MyPhotos/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  MyPhotos
+//
+//  Created by s5014219 on 20/04/2016.
+//  Copyright © 2016 s5014219. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
