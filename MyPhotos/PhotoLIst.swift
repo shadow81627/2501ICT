4ac353d1807afd33f7c4dbabac9894609314a4ba
s5014219 <s5014219@pc027709.staff.ad.griffith.edu.au>
2016-04-29 09:11:18 +1000

    added save and load for the entries list

--- MyPhotos/PhotoLIst.swift
+++ MyPhotos/PhotoLIst.swift
@@ -10,4 +10,25 @@ import Foundation
 
 class PhotoList {
     var entries = [Photo]()
+    
+    //writes all of the entries to afile for persistent saving of photos
+    func save(){
+        for entry in entries{
+            let propertyList = entry.propertyList()
+            let documentsDirectory = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as NSString
+            let fileName = documentsDirectory.stringByAppendingPathComponent("photo.plist");
+            propertyList.writeToFile(fileName, atomically: true)
+        }
+    }
+    
+    //loads photos from a file into the entries array
+    func load(){
+        let documentsDirectory = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as NSString
+        let fileName = documentsDirectory.stringByAppendingPathComponent("photo.plist")
+        let optionalArray = NSDictionary(contentsOfFile: fileName)
+        //for pl in optionalArray! {
+            let photo = Photo(propertyList: optionalArray!)
+            entries.insert(photo, atIndex: 0)
+        //}
+    }
 }
