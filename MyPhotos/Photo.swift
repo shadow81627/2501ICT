f19e2a109c210f4b18e4fb86a25cf161410bd13a
s5014219 <s5014219@n44p234z027701.student.ad.griffith.edu.au>
2016-04-28 11:41:47 +1000

    added load and save property lists to Photo

--- MyPhotos/Photo.swift
+++ MyPhotos/Photo.swift
@@ -15,10 +15,24 @@ class Photo {
     var url: String
     var imageData: NSData?
     
+    //creates a new Photo with given details
     init(title: String? = nil, tag: [String]? = nil, url: String, imageData: NSData? = nil){
         self.title = title
         self.tag = tag
         self.url = url
         self.imageData = imageData
+    }
+    
+    //creates a photo from saved photo data
+    convenience init(propertyList: NSDictionary){
+        self.init(title: " ", tag: [" "], url: " ")
+        title = propertyList["title"] as? String
+        tag = propertyList["tag"] as? [String]
+        url = (propertyList["url"] as? String)!
+    }
+    
+    //returns a dictionaray of the photo details
+    func propertyList() -> NSDictionary {
+        return ["title": title!, "tag": tag!, "url": url]
     }
 }
