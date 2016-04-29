280fd7cfc6b5b72e345f15b87c487c240646cf3b
s5014219 <s5014219@pc027709.staff.ad.griffith.edu.au>
2016-04-29 11:05:24 +1000

    fixed save and load to be better and work

--- MyPhotos/PhotoLIst.swift
+++ MyPhotos/PhotoLIst.swift
@@ -13,22 +13,17 @@ class PhotoList {
     
     //writes all of the entries to afile for persistent saving of photos
     func save(){
-        for entry in entries{
-            let propertyList = entry.propertyList()
+        let propertyList: NSArray = entries.map{ $0.propertyList() }
             let documentsDirectory = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as NSString
-            let fileName = documentsDirectory.stringByAppendingPathComponent("photo.plist");
+            let fileName = documentsDirectory.stringByAppendingPathComponent("photo.plist")
             propertyList.writeToFile(fileName, atomically: true)
-        }
     }
     
     //loads photos from a file into the entries array
     func load(){
         let documentsDirectory = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as NSString
         let fileName = documentsDirectory.stringByAppendingPathComponent("photo.plist")
-        let optionalArray = NSDictionary(contentsOfFile: fileName)
-        //for pl in optionalArray! {
-            let photo = Photo(propertyList: optionalArray!)
-            entries.insert(photo, atIndex: 0)
-        //}
+        let fileContent = NSArray(contentsOfFile: fileName) as! Array<NSDictionary>
+        entries = fileContent.map { Photo(propertyList: $0)}
     }
 }
