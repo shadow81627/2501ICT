e581275db1213fe0775c788c8c6649a2a1a87565
s5014219 <s5014219@pc027709.staff.ad.griffith.edu.au>
2016-05-04 13:46:20 +1000

    added PhotoViewController

---
+++ MyPhotos/PhotoViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoViewController.swift
+//  MyPhotos
+//
+//  Created by s5014219 on 4/05/2016.
+//  Copyright © 2016 s5014219. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController{
+    
+    
+}
