a0fb3b8fdb5fc842c70bf2b771461f47d8b51053
s5014219 <s5014219@n44p234z027709.student.ad.griffith.edu.au>
2016-05-11 12:44:32 +1000

    added outlets for the textfield and the web view

--- WebViewer/ViewController.swift
+++ WebViewer/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var urlField: UITextField!
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
