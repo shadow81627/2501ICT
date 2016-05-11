3db09ae5eb60c5e549f2d21baf02c32c6852a093
s5014219 <s5014219@n44p234z027709.student.ad.griffith.edu.au>
2016-05-11 12:28:42 +1000

    Initial Commit

---
+++ WebViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebViewer
+//
+//  Created by s5014219 on 11/05/2016.
+//  Copyright © 2016 s5014219. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
