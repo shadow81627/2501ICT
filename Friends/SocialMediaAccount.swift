b12449419ea89f15e469de2a01f9558c6762e961
s5014219 <s5014219@pc027709.staff.ad.griffith.edu.au>
2016-05-12 11:46:20 +1000

    added social media account

---
+++ Friends/SocialMediaAccount.swift
@@ -0,0 +1,13 @@
+//
+//  SocialMediaAccount.swift
+//  Friends
+//
+//  Created by s5014219 on 12/05/2016.
+//  Copyright © 2016 s5014219. All rights reserved.
+//
+
+import Foundation
+
+class SocialMediaAccount {
+    
+}
