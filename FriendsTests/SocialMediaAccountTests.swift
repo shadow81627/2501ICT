5e0d42713cf698990b5667cd114ad87518e1c723
s5014219 <s5014219@n44p234z027709.student.ad.griffith.edu.au>
2016-05-13 09:31:36 +1000

    added tests

---
+++ FriendsTests/SocialMediaAccountTests.swift
@@ -0,0 +1,35 @@
+//
+//  SocialMediaAccountTests.swift
+//  Friends
+//
+//  Created by s5014219 on 13/05/2016.
+//  Copyright © 2016 s5014219. All rights reserved.
+//
+
+import XCTest
+
+class SocialMediaAccountTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
