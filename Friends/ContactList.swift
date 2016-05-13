fd11532b1363d27df2ab16d099750cdbb5b158d1
s5014219 <s5014219@n44p234z027709.student.ad.griffith.edu.au>
2016-05-13 10:32:08 +1000

    added ContactList

---
+++ Friends/ContactList.swift
@@ -0,0 +1,13 @@
+//
+//  ContactList.swift
+//  Friends
+//
+//  Created by s5014219 on 13/05/2016.
+//  Copyright © 2016 s5014219. All rights reserved.
+//
+
+import Foundation
+
+class ContactList {
+    var entries = [Contact]()
+}
