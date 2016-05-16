88049aea73dcc7373ab5097247dc97ee4ec74b5f
s5014219 <s5014219@n44p234z027709.student.ad.griffith.edu.au>
2016-05-17 08:25:10 +1000

    added social media accounts array

--- Friends/Contact.swift
+++ Friends/Contact.swift
@@ -25,8 +25,11 @@ class Contact: NSObject {
     //Sets the url and then sets the image data witht he given url
     var imageURL: String
     
+    //The list ofsocial media accounts this contact has
+    var accounts: [SocialMediaAccount]?
+    
     //initialiser for the Contact class takes a String address, firstname, lastname NSURL imageURL and can take image data.
-    init(address: String, firstName: String, lastName: String, image: NSData? = nil, imageURL: String){
+    init(address: String, firstName: String, lastName: String, image: NSData? = nil, imageURL: String, accounts: [SocialMediaAccount]? = nil){
         self.address = address
         self.firstName = firstName
         self.lastName = lastName
@@ -46,11 +49,17 @@ class Contact: NSObject {
         firstName = (propertyList["firstName"] as? String)!
         lastName = (propertyList["lastName"] as? String)!
         imageURL = (propertyList["imageURL"] as? String)!
+        accounts = (propertyList["accounts"] as? [SocialMediaAccount])
     }
     
-    //returns a dictionaray of contact details address, firstName, lastName and imageURL
+    //returns a dictionaray of contact details address, firstName, lastName, imageURL and accounts if there are any
     func propertyList() -> NSDictionary {
-        return ["address": address, "firstName": firstName, "lastName": lastName, "imageURL": imageURL]
+        if accounts!.count > 0 {
+            return ["address": address, "firstName": firstName, "lastName": lastName, "imageURL": imageURL, "accounts": accounts!]
+        }else{
+            return ["address": address, "firstName": firstName, "lastName": lastName, "imageURL": imageURL, "accounts": [""]]
+        }
+
     }
     
 }
